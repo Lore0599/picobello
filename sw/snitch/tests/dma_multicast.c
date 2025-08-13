61ecfa12d5d8c1ba15cb563983f3f9aa22ab5b8d
Lorenzo Leone <lleone@iis.ee.ethz.ch>
2025-08-13 10:36:07 +0200

    sw: Add binary tree approach + python plotting

--- sw/snitch/tests/dma_multicast.c
+++ sw/snitch/tests/dma_multicast.c
@@ -15,7 +15,16 @@
 // to multicast some DMA trasnefers over multiple clusters in the Picobello
 // system. By driving some parameters externally, it's possible to get
 // the performance results.
-
+//
+// Comment:
+// The use of WFI and global_barrier together can create some issues due to
+// non unique CLINT signal mapping. A possibility would be not to send the
+// clusters into WFI, however this will create complicances in teh wake up routine.
+// For instance, each cluster would nee to poll the DMA transfer detsination
+// until teh expected value is written, and only then it can move to the next
+// algorithm step. However, since the code is run twice to pre-heat the cache,
+// in the second iteration each cluster will already have the expected results
+// and will start the transfer immediately.
 
 #include <stdint.h>
 #include "pb_addrmap.h"
@@ -26,6 +35,7 @@
 typedef enum {
     SW_UNOPT,
     SW_OPT,
+    SW_OPT2,
     HW_MCAST
 } mode_t;
 
@@ -45,7 +55,17 @@ typedef enum {
 #define N_CLUSTERS_TO_USE 8
 #endif
 
+#define LOG2_COL 2
+#define LOG2_ROW 2
+
 #define BCAST_MASK_ACTIVE ((N_CLUSTERS_TO_USE - 1) << 18)
+
+#define cluster_is_in_column(col) \
+    __cluster_is_in_column_api((col) - 1)
+
+static inline int __cluster_is_in_column_api(int col){
+    return ((snrt_cluster_idx() >> LOG2_ROW) == col);
+}
 
 //-------------------
 //| Mcast functions |
@@ -75,7 +95,7 @@ static inline void cluster_broadcast_sw_unopt(uint32_t cluster_idx, void* dst, v
 // broadcast over its own row
 static inline void cluster_broadcast_sw_opt(uint32_t cluster_idx, void* dst, void* src, size_t size) {
     if (snrt_is_dm_core() && (snrt_cluster_idx() == cluster_idx)) {
-        for (int row_cl = 1; row_cl < 4; row_cl ++){
+        for (int row_cl = 0; row_cl < 4; row_cl ++){
             snrt_dma_start_1d(snrt_remote_l1_ptr(dst, 0, row_cl), src, size);
             snrt_dma_wait_all();
         }
@@ -86,8 +106,10 @@ static inline void cluster_broadcast_sw_opt(uint32_t cluster_idx, void* dst, voi
         // any network congestion that might affect the final results.
 
         // Wake up clusters in col 0
-        for (int row_cl = 1; row_cl < 4; row_cl ++){
-            snrt_cluster(row_cl)->peripheral_reg.cl_clint_set.f.cl_clint_set = 0x1ff;
+        if (N_CLUSTERS_TO_USE > 4) {
+            for (int row_cl = 1; row_cl < 4; row_cl ++){
+               snrt_cluster(row_cl)->peripheral_reg.cl_clint_set.f.cl_clint_set = 0x1ff;
+        }
         }
         // Send data from cluster 0 to row 0
         for (int col_cl = 1; col_cl < N_CLUSTERS_TO_USE/4; col_cl ++){
@@ -95,8 +117,8 @@ static inline void cluster_broadcast_sw_opt(uint32_t cluster_idx, void* dst, voi
                                 src, size);
             snrt_dma_wait_all();
         }
-    } else if (snrt_is_dm_core() && (snrt_cluster_idx() < 4)) {
-        // The clusters in the first row will wait to be waked up by cluster 0
+    } else if (snrt_is_dm_core() && cluster_is_in_column(1) && N_CLUSTERS_TO_USE > 4) {
+        // The clusters in the first column will wait to be waked up by cluster 0
         // and only then will isue the multicast transaction to the row
         snrt_wfi();
         snrt_int_clr_mcip();
@@ -106,6 +128,89 @@ static inline void cluster_broadcast_sw_opt(uint32_t cluster_idx, void* dst, voi
                                 dst, size);
             snrt_dma_wait_all();
         }
+    }
+}
+
+// SW_OPT2: to maximize the paralle work, cluster 0 multicast to cluster 2,
+// then simultaneously cluster 0 and 2 multicast to 1 and 3 respectively.
+// The same strided approach is used column-wise.
+// This approach is afster for NUM_CLUSTERS = 16 only.
+// or this reason the following code is not generic enough to handle NUM_CLUSTERS < 16.
+static inline void cluster_broadcast_sw_opt2(uint32_t cluster_idx, void* dst, void* src, size_t size) {
+
+    if (snrt_is_dm_core() && (snrt_cluster_idx() == cluster_idx)) {
+        // DMA transfer to cluster 0
+        snrt_dma_start_1d(snrt_remote_l1_ptr(dst, 0, 0), src, size);
+        snrt_dma_wait_all();
+
+        // DMA transfer to cluster 2 anf then wake up and move one
+        snrt_dma_start_1d(snrt_remote_l1_ptr(dst, 0, 2), src, size);
+        snrt_dma_wait_all();
+        snrt_cluster(2)->peripheral_reg.cl_clint_set.f.cl_clint_set = 0x1ff;
+
+        // DMA transfer to cluster 1 and then wake up and move to the row
+        snrt_dma_start_1d(snrt_remote_l1_ptr(dst, 0, 1), src, size);
+        snrt_dma_wait_all();
+        // WWake up cluster 1 only if you need a second iteration for the next column
+        if (N_CLUSTERS_TO_USE > 4){
+            snrt_cluster(1)->peripheral_reg.cl_clint_set.f.cl_clint_set = 0x1ff;
+        }
+
+        if (N_CLUSTERS_TO_USE > 8){
+            // DMA transfer to cluster 8 and then wake up and move to 4
+            snrt_dma_start_1d(snrt_remote_l1_ptr(dst, 0, 8), src, size);
+            snrt_dma_wait_all();
+            snrt_cluster(8)->peripheral_reg.cl_clint_set.f.cl_clint_set = 0x1ff;
+        }
+
+        if (N_CLUSTERS_TO_USE > 4) {
+            // DMA transfer to cluster 4. No need to wake it up now since
+            // it is not involved in any transfer
+            snrt_dma_start_1d(snrt_remote_l1_ptr(dst, 0, 4), src, size);
+            snrt_dma_wait_all();
+        }
+
+    } else if (snrt_is_dm_core() && cluster_is_in_column(1)) {
+        // The clusters in the first column will wait to be waked up by cluster 0
+        // and only then will isue the multicast transaction to the row
+        if (N_CLUSTERS_TO_USE > 4 || snrt_cluster_idx() == 2) {
+            snrt_wfi();
+            snrt_int_clr_mcip();
+        }
+
+        // Only cluster 2 is repsonsible to transfer also to cluster 3
+        if (snrt_cluster_idx() == 2) {
+            // DMA transfer to cluster 8 and then wake up and move to 4
+            snrt_dma_start_1d(snrt_remote_l1_ptr(dst, snrt_cluster_idx(), snrt_cluster_idx() + 1), dst, size);
+            snrt_dma_wait_all();
+            snrt_cluster(snrt_cluster_idx() + 1)->peripheral_reg.cl_clint_set.f.cl_clint_set = 0x1ff;
+        }
+
+        if (N_CLUSTERS_TO_USE > 8){
+            // All clusters in the first column need to send the data with
+            // a stride of 8 first and 4 after.
+            // DMA transfer to cluster 8 and then wake up and move to 4
+            snrt_dma_start_1d(snrt_remote_l1_ptr(dst, snrt_cluster_idx(), snrt_cluster_idx() + 8), dst, size);
+            snrt_dma_wait_all();
+            snrt_cluster(snrt_cluster_idx() + 8)->peripheral_reg.cl_clint_set.f.cl_clint_set = 0x1ff;
+        }
+
+        if (N_CLUSTERS_TO_USE > 4) {
+            // Then the clusters in the first column can send the transfer with stride 4,
+            // i.e. to the cluster in he same row but next column.
+            snrt_dma_start_1d(snrt_remote_l1_ptr(dst, snrt_cluster_idx(), snrt_cluster_idx() + 4), dst, size);
+            snrt_dma_wait_all();
+        }
+
+
+    } else if (snrt_is_dm_core() && cluster_is_in_column(3) && N_CLUSTERS_TO_USE > 8) {
+        // The clusters in the column 3 will wait to be waked up by cluster
+        // and only then will isue the multicast transaction to the row
+        snrt_wfi();
+        snrt_int_clr_mcip();
+
+        snrt_dma_start_1d(snrt_remote_l1_ptr(dst, snrt_cluster_idx(), snrt_cluster_idx() + 4), dst, size);
+        snrt_dma_wait_all();
     }
 }
 
@@ -122,6 +227,9 @@ static inline void dma_broadcast_to_clusters(void* dst, void* src, size_t size) 
         case SW_OPT:
             cluster_broadcast_sw_opt(0, dst, src, size);
             break;
+        case SW_OPT2:
+            cluster_broadcast_sw_opt2(0, dst, src, size);
+            break;
     }
 }
 
@@ -130,26 +238,11 @@ static inline int cluster_participates_in_bcast(int i) {
     return (i < N_CLUSTERS_TO_USE);
 }
 
-
 static inline void broadcast_wrapper(void* dst, void* src, size_t size) {
     snrt_global_barrier();
     snrt_mcycle();
     dma_broadcast_to_clusters(dst, src, size);
     snrt_mcycle();
-    // Put clusters who don't participate in the broadcast to sleep, as if
-    // they proceed directly to the global barrier, they will interfere with
-    // the other clusters, by sending their atomics on the narrow interconnect.
-    // if (!cluster_participates_in_bcast(snrt_cluster_idx())) {
-    //     snrt_wfi();
-    //     snrt_int_clr_mcip();
-    // }
-    // // Wake these up when cluster 0 is done
-    // else if ((snrt_cluster_idx() == 0) && snrt_is_dm_core()) {
-    //     for (int i = 0; i < snrt_cluster_num(); i++) {
-    //         if (!cluster_participates_in_bcast(i))
-    //             snrt_cluster(i)->peripheral_reg.cl_clint_set.f.cl_clint_set = 0x1ff;
-    //     }
-    // }
 }
 
 //
